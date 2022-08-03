03476e2bccb404b17be8c26eba0c3a734adfce11
armancse-dev <armanhossaincse7@gmail.com>
2022-08-03 22:33:23 +0600

    Router, route, path, component, not found, exact

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
-// import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path='/home' element={<Home/>}></Route>
           <Route path='/about' element={<About/>}></Route>
           <Route path='/friends' element={<Friends/>}></Route>
           <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='*' element={<NotFound/>}></Route>
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
 
-      <Friends></Friends>
+     
     </div>
   );
 }
